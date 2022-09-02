9642286f1fc4d96820845b7d3c79c0ac6aac808d
Wmac23 <72547887+Wmac23@users.noreply.github.com>
2022-09-02 11:10:15 +0200

    Add files via upload

---
+++ flow1/script.js
@@ -0,0 +1,30 @@
+function calculate () {
+var username = document.getElementById("username").value
+var height = document.getElementById("height").value
+var weight = document.getElementById("weight").value
+var bmi = (weight/(height*height))*10000;
+var bmindex = bmi.toFixed(2);
+
+
+const person ={
+    name: username,
+    weight: weight,
+    height: height,
+    bmindex: bmindex
+};
+    document.getElementById("result").innerHTML = "Name: " + person.name + " <br>  Weight " + person.weight + " <br>  Height " + person.height + " <br> BMI: " + person.bmindex;
+    console.log(person);
+    document.getElementById("myForm").reset();
+}
+
+
+
+
+
+
+
+
+
+
+
+
